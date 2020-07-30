09a9bc39b6d7a75165f110ce70436995a6a4e120
tmszmp <68904713+tmszmp@users.noreply.github.com>
2020-07-30 14:48:03 +0200

    Add files via upload

---
+++ sync-all.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+cd /home/tim_schatz/wetter
+git pull
+docker build . -t gcr.io/proud-effect-284706/wetter
+docker push gcr.io/proud-effect-284706/wetter
+cd /home/tim_schatz/cities
+git pull
+#docker build . -t gcr.io/proud-effect-284706/cities
+#docker push gcr.io/proud-effect-284706/cities
+kubectl apply -f /home/tim_schatz/test.yaml
+kubectl rollout restart deployment.apps/wetter-app
+kubectl rollout restart deployment.apps/cities-app
