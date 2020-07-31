2f48d3c6a721ce8471eb405131719d040f71a43d
tmszmp <68904713+tmszmp@users.noreply.github.com>
2020-07-31 13:04:46 +0200

    Update sync-all-prod.sh

--- sync-all-prod.sh
+++ sync-all-prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-kubectl apply -f data/data.yaml
+kubectl apply -f data/data-prod.yaml
 kubectl rollout restart deployment.apps/wetter-app
 kubectl rollout restart deployment.apps/cities-app
 kubectl get pods
