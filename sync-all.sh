64627ea5ed7f5b2293fff92e32302da87768db56
tmszmp <68904713+tmszmp@users.noreply.github.com>
2020-07-31 07:30:29 +0200

    Update sync-all.sh

--- sync-all.sh
+++ sync-all.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
-docker build . -t gcr.io/proud-effect-284706/wetter
-docker push gcr.io/proud-effect-284706/wetter
 kubectl apply -f data.yaml
 kubectl rollout restart deployment.apps/wetter-app
 kubectl rollout restart deployment.apps/cities-app
