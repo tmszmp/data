c0b4b5ad720813542e6ff790e90b7d45661f8f3c
tmszmp <68904713+tmszmp@users.noreply.github.com>
2020-07-31 08:37:57 +0200

    Update sync-all.sh

--- sync-all.sh
+++ sync-all.sh
@@ -2,4 +2,7 @@
 kubectl apply -f data/data.yaml
 kubectl rollout restart deployment.apps/wetter-app
 kubectl rollout restart deployment.apps/cities-app
+kubectl get pods
+sleep 20
+kubectl get pods
 kubectl get services
